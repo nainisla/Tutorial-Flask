b47fc3235dc1b9f74511b5a9b020eb934c43c86c
nainisla <nainisla50@gmail.com>
2025-03-26 11:59:41 -0300

    pagina principipal y 2 rutas

--- app.py
+++ app.py
@@ -3,5 +3,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def principal():
+    return """
+        <a href='/hola'>saludar</a>
+        <a href='/chau'>despedir</a>
+    """
+
+@app.route("/hola")
+def saludar():
     return "<p>Hello, World!</p>"
+
+@app.route("/chau")
+def despedir():
+    return "<p>Bye, World!</p>"
+
