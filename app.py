3830117afd5b3ab2488ca72488e0a9dfde1bf45c
solp3rez <axelweath@gmail.com>
2025-04-16 11:55:46 -0300

    Conexion a la base de datos

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
-from flask import Flask
+from flask import Flask , url_for
+import sqlite3
 
 app = Flask(__name__)
+
+db=None
+
+def AbrirConexion():
+    global db
+    db = sqlite3.connect('instance/datos.sqlite')
+    db.row_factory= sqlite3.Row
+    return db
+
+def CerrarConexion():
+    global db
+    if db is not None:
+        db.close()
+        db=None
+
+@app.route("/usuarios/")
+def ObtenerGente():
+    global db
+    conexion = AbrirConexion()
+    cursor = conexion.cursor()
+    cursor.execute('SELECT * FROM usuarios')
+    resultado = cursor.fetchall()
+    CerrarConexion()
+    fila = [dict(row) for row in resultado]
+    return str(fila)
 
 @app.route("/")
 def principal():
     return """
-        <a href='/hola'>saludar</a>
-        <a href='/chau'>despedir</a>
+        <a href="{url_for('saludar')}">Saludar</a><br>
+        <a href="{url_for('despedir')}">Despedir</a><br>
+        <a href="{url_for('dado', caras=6)}">Tirar dado de 6</a><br>
+        <a href="{url_for('dado', caras=20)}">Tirar dado de 20</a><br>
     """
 
 @app.route("/hola")
@@ -22,4 +50,5 @@ def dado(caras):
     from random import randint
     numero = randint(1,caras)
     return f"<h2>Dado de {caras} caras, salio {numero}!</h2>"
+
 
