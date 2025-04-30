cbdaa492323d95a3a47412ee7cc40a5cfd8f505f
joel012930 <joelcari1807@gmail.com>
2025-04-30 12:11:03 -0300

    agregar,borrar usuario,gmail

--- app.py
+++ app.py
@@ -3,30 +3,66 @@ import sqlite3
 
 app = Flask(__name__)
 
-db=None
+db = None
 
-def AbrirConexion():
-    global db
-    db = sqlite3.connect('instance/datos.sqlite')
-    db.row_factory= sqlite3.Row
-    return db
+def dict_factory(cursor, row):
+  """Arma un diccionario con los valores de la fila."""
+  fields = [column[0] for column in cursor.description]
+  return {key: value for key, value in zip(fields, row)}
 
-def CerrarConexion():
-    global db
-    if db is not None:
-        db.close()
-        db=None
+def abrirConexion():
+   global db
+   db = sqlite3.connect("instance/datos.sqlite")
+   db.row_factory = dict_factory
 
-@app.route("/usuarios/")
-def ObtenerGente():
-    global db
-    conexion = AbrirConexion()
-    cursor = conexion.cursor()
-    cursor.execute('SELECT * FROM usuarios')
-    resultado = cursor.fetchall()
-    CerrarConexion()
-    fila = [dict(row) for row in resultado]
-    return str(fila)
+def cerrarConexion():
+   global db
+   db.close()
+   db = None
+
+@app.route("/test-db")
+def testDB():
+   abrirConexion()
+   cursor = db.cursor()
+   cursor.execute("SELECT COUNT(*) AS cant FROM usuarios; ")
+   res = cursor.fetchone()
+   registros = res["cant"]
+   cerrarConexion()
+   return f"Hay {registros} registros en la tabla usuarios"
+
+@app.route("/agregar")
+def agregar_usuario():
+    abrirConexion()
+    cursor = db.cursor()
+    #usuario = request.args.get("usuario")  Para agregar desde la URL
+    #email = request.args.get("email")      http://127.0.0.1:5000/agregar?usuario=Pedro&email=pedro@mail.com
+    usuario ="juan" 
+    email ="juan@gmail.com"
+    cursor.execute("INSERT INTO usuarios(usuario,email) VALUES (?,?)",(usuario,email))
+    db.commit()  # muy importante para guardar los cambios
+    cerrarConexion()
+    return f"Usuario {usuario} agregado con email {email}"
+
+@app.route("/borrar")
+def borrar_usuario():
+    abrirConexion()
+    cursor = db.cursor()
+    #id = request.argst.get("id")    para agregar desde la url
+    id = 2                          #http://127.0.0.1:5000/borrar?id=2
+    cursor.execute("DELETE FROM usuarios WHERE id =?",(id,))
+    db.commit()
+    cerrarConexion()
+    return f"se Borro el usuario con ID {id}"
+
+@app.route("/MostrarPorID/<int:id>")
+def nombre_email_usurio(id):
+    abrirConexion()
+    cursor = db.cursor()
+    #http://127.0.0.1:5000/2
+    cursor.execute("SELECT usuario ,email FROM usuarios WHERE id = ?", (id,))
+    resultado = cursor.fetchone()
+    cerrarConexion()
+    return f"mostrar{resultado["usuario"]} y email {resultado["email"]} "
 
 @app.route("/")
 def principal():
